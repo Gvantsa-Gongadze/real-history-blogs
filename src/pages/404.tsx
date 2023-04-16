8deea834664671358f7c0662e334eae917b45c33
Gvantsa5 <ggong2016@agruni.edu.ge>
2023-04-16 22:13:51 +0400

    feat: creates next js app and instaled contetful cms

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+	return (
+		<>
+			<div>
+				<div>
+					<div>
+						<span>Oops!</span>
+					</div>
+					<div>This page does not seem to exist, sorry.</div>
+				</div>
+			</div>
+		</>
+	)
+}
